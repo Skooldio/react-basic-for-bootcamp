0d449ce3c81fc428641b2ccd407a2068f5bb17aa
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2024-09-21 19:14:18 +0700

    ✨ time formatter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useState, useEffect } from "react";
 
+const formatTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+};
+
 function App() {
   const [initialSessionTimer, setInitialSessionTimer] = useState(25);
   const [initialBreakTimer, setInitialBreakTimer] = useState(5);
-  const [sessionTimer, setSessionTimer] = useState(initialSessionTimer);
-  const [breakTimer, setBreakTimer] = useState(initialBreakTimer);
+  const [sessionTimer, setSessionTimer] = useState(initialSessionTimer * 60);
+  const [breakTimer, setBreakTimer] = useState(initialBreakTimer * 60);
 
   useEffect(() => {
-    setSessionTimer(initialSessionTimer);
+    setSessionTimer(initialSessionTimer * 60);
   }, [initialSessionTimer]);
   useEffect(() => {
-    setBreakTimer(initialBreakTimer);
+    setBreakTimer(initialBreakTimer * 60);
   }, [initialBreakTimer]);
 
   return (
@@ -20,7 +26,7 @@ function App() {
       <div className="bg-redwood flex flex-col justify-center items-center w-4/5">
         <div className=" w-1/3 flex flex-col justify-center items-center p-2">
           <h2>Session</h2>
-          <h2>{sessionTimer}</h2>
+          <h2>{formatTime(sessionTimer)}</h2>
           <div className="flex justify-between w-full">
             <button className="btn">start</button>
             <button className="btn">reset</button>
